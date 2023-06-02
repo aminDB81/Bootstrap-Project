24818fe58ac616578c2335c63c14c3aeb08c8ec2
aminDB81 <aminbaluchi67@gmail.com>
2023-06-02 23:24:55 +0330

    Add two more sections

---
+++ vendors/Lottie/lottie.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets1.lottiefiles.com/packages/lf20_kdikdv8b.json' // lottie file path
+  })
